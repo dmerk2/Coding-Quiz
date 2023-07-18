25c2361569f4c97fbffe344444382d66cfc77dab
Dan Merkin <dan.merkin@gmail.com>
2023-07-18 14:45:03 -0400

    created function to hide cards once start button is pushed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,83 @@
-let timeLeft = document.getElementById("timeLeft");
-let timeEl = document.getElementById("time");
+let time = document.getElementById("time");
+let startButton = document.getElementById("startButton");
+// Classname?????
+let answerOptions = document.getElementsByClassName("option-button");
+let startCard = document.getElementById("startCard");
+let questionCard = document.getElementById("questions");
+let resultCard = document.getElementById("result");
+
+//Questions, options and answers are stored in an array
+let questions = [
+  {
+    questionText: "Where do you place an external JavaScript file?",
+    options: ["1. <h1>", "2. <js>", "3. <script>", "4. <head>"],
+    answer: "3. <script>",
+  },
+  {
+    questionText: "What does the DOM stand for?",
+    options: [
+      "1. do overnight modules",
+      "2. document object model",
+      "3. department of modeling",
+      "4. do over methods",
+    ],
+    answer: "2. document object model",
+  },
+  {
+    questionText: "String values must be enclosed in what?",
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    answer: "3. quotes",
+  },
+  {
+    questionText:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console.log",
+    ],
+    answer: "4. console.log",
+  },
+  {
+    questionText: "Arrays in Javascript can be used to store __________.",
+    options: ["1. numbers", "2. booleans", "3. strings", "4. all of the above"],
+    answer: "4. all of the above",
+  },
+];
+
+// Timer for the game to countdown
+let timeLeft = 60;
 
 const countdown = () => {
-  var timeLeft = 10;
-
-  var timeInterval = setInterval(function () {
+  let timeInterval = setInterval(() => {
     if (timeLeft > 0) {
-      timeEl.textContent = 'Time Left: ' + timeLeft;
+      time.textContent = "Time Left: " + timeLeft;
       timeLeft--;
     } else {
-      timeEl.textContent = 'Time is up!';
+      time.textContent = "Time is up!";
       clearInterval(timeInterval);
     }
   }, 1000);
-}
-countdown();
+};
+
+// When the start button is clicked, begin the timer
+startButton.addEventListener("click", () => {
+  if (startButton) {
+    countdown();
+    hideCards();
+  }
+});
+
+// NOT WORKING!
+// Only first index is responding
+let answers = answerOptions[0];
+answers.addEventListener("click", () => {
+  console.log("clicked");
+});
+
+const hideCards = () => {
+  startCard.setAttribute("hidden", true);
+  questionCard.removeAttribute("hidden", true);
+  resultCard.removeAttribute("hidden", true);
+};
