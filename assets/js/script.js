9a84af4a73d2d0f0ec5739fa800774b2707f970c
Dan Merkin <dan.merkin@gmail.com>
2023-07-19 22:00:12 -0400

    adding function to move to the following question when answer button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let answerOptions = document.getElementsByClassName("option-button");
 let startCard = document.getElementById("startCard");
 let questionCard = document.getElementById("questions");
 let resultCard = document.getElementById("result");
+let questionText = document.getElementById("question-text");
 
 //Questions, options and answers are stored in an array
 let questions = [
@@ -66,6 +67,7 @@ startButton.addEventListener("click", () => {
   if (startButton) {
     countdown();
     hideCards();
+    renderQuestion();
   }
 });
 
@@ -74,10 +76,25 @@ startButton.addEventListener("click", () => {
 let answers = answerOptions[0];
 answers.addEventListener("click", () => {
   console.log("clicked");
+  nextQuestion();
+  renderQuestion();
 });
 
+// Setting attribute to hide cards
 const hideCards = () => {
   startCard.setAttribute("hidden", true);
   questionCard.removeAttribute("hidden", true);
   resultCard.removeAttribute("hidden", true);
 };
+
+let currentQuestion = 0;
+
+const nextQuestion = () => {
+  if (currentQuestion < questions.length - 1) {
+    currentQuestion++;
+  }
+};
+
+const renderQuestion = () => {
+  questionText.textContent = questions[currentQuestion].questionText;
+}
