aed0df658076b71c0acadf7e72a61da4ea4379c6
Dan Merkin <dan.merkin@gmail.com>
2023-07-25 14:25:46 -0400

    adding function to show if answers are correct or incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,28 +13,28 @@ let restartEl = document.getElementById("restart");
 let backEl = document.getElementById("back");
 let saveEl = document.getElementById("save");
 let userName = document.getElementById("userName");
-let savedUser = document.getElementById("savedUser");
-let savedScore = document.getElementById("savedScore");
 let clear = document.getElementById("clear");
+let result = document.getElementById("result-text");
+let savedScore = document.getElementById("savedScore");
 let storeInitials = [];
 let timeInterval;
 
 // Questions, options and answers are stored in an array
 let questions = [
   {
-    questionText: "Where do you place an external JavaScript file?",
-    options: ["1. <h1>", "2. <js>", "3. <script>", "4. <head>"],
-    answer: "3. <script>",
+    questionText: "What is the correct way to declare a variable in JavaScript?",
+    options: ["1. variable = 10;", "2. var = 10;", "3. let variable = 10;", "4. int variable = 10;"],
+    answer: "3. let variable = 10;",
   },
   {
-    questionText: "What does the DOM stand for?",
+    questionText: "Which method is used to add an element to the end of an array in JavaScript?",
     options: [
-      "1. do overnight modules",
-      "2. document object model",
-      "3. department of modeling",
-      "4. do over methods",
+      "1. array.add(element);",
+      "2. array.insertLast(element);",
+      "3. array.push(element);",
+      "4. array.addElement(element);",
     ],
-    answer: "2. document object model",
+    answer: "3. array.push(element);",
   },
   {
     questionText: "String values must be enclosed in what?",
@@ -87,19 +87,38 @@ const renderQuestionText = () => {
   questionText.textContent = questions[currentQuestion].questionText;
   for (let i = 0; i < questions[i].options.length; i++) {
     const btn = document.createElement("button");
+    btn.classList.add("answerBtn");
     btn.textContent = questions[currentQuestion].options[i];
     answerOptions.append(btn);
   }
+};
+
+// Show whether the answer is correct or incorrect for 2 seconds
+// LAST ANSWER DOES NOT DISPLAY
+const displayAnswer = () => {
+  let timeInterval = 2;
+  timeInterval = setInterval(() => {
+    if (timeInterval > 0) {
+      resultCard.classList.remove("hide");
+    } else {
+      resultCard.classList.add("hide");
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 };
 
 // Check if the answer is correct and if not remove 5 seconds
 const checkAnswer = (userChoice) => {
   if (userChoice === questions[currentQuestion].answer) {
     currentQuestion++;
+    result.textContent = "Correct";
+    displayAnswer();
     renderQuestionText();
   } else {
     currentQuestion++;
     timeLeft -= 5;
+    result.textContent = "Incorrect";
+    displayAnswer();
     renderQuestionText();
   }
 };
@@ -116,17 +135,30 @@ const saveHighScore = (initials) => {
   storeInitials = JSON.parse(localStorage.getItem("highScores")) || [];
   const user = {
     initials: initials,
-    score: timeLeft + 1,
-  }
+    score: timeLeft,
+  };
   storeInitials.push(user);
   localStorage.setItem("highScores", JSON.stringify(storeInitials));
+  // Store score and initials to highscores
+  let pEl = document.createElement("p");
+  pEl.textContent = user.initials + ": " + user.score;
+  savedScore.append(pEl);
 };
+
+let allScores = document.getElementById("allScores");
+// Save scores to localstorage and high scores
+const saveToHighScoreCard = () => {
+  user = JSON.parse(localStorage.getItem("highScores"));
+  user.push(allScores);
+  console.log("user", user);
+}
+allScores.textContent = "Test";
 
 highScoresButton.addEventListener("click", () => {
   highScoreCard.classList.replace("hide", "show");
+  resultCard.classList.replace("show", "hide");
+  scoreCard.classList.replace("show", "hide");
   startCard.setAttribute("hidden", true);
-  resultCard.setAttribute("hidden", true);
-  scoreCard.setAttribute("hidden", true);
 });
 
 // Reload window to starting card
@@ -143,6 +175,7 @@ backEl.addEventListener("click", () => {
 saveEl.addEventListener("click", () => {
   const userInitials = userName.value;
   saveHighScore(userInitials);
+  saveToHighScoreCard();
 });
 
 // When the start button is clicked, begin the timer
